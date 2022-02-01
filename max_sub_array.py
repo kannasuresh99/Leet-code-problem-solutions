a4e5fa4e57436d7ee8ca4f43de4d96e77fa331a4
kannasuresh99 <kannappansuresh99@gmail.com>
2022-02-01 21:24:19 +0530

    health problem fix

---
+++ max_sub_array.py
@@ -0,0 +1,15 @@
+#my solution
+class Solution:
+    def maxSubArray(self,nums):
+        index = 0
+        while index < len(nums):
+            
+
+
+
+
+
+
+res = Solution()
+result = res.maxSubArray([1,-3,2,1,-1])
+print(result)
