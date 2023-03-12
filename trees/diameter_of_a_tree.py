6bd7fb4deeca56226e332bcb2a0485e87042c889
Kannappan Suresh <kannappansuresh99@gmail.com>
2023-03-12 12:15:01 +0530

    A woman says 1000 things out of that 1001 things are nothing but lies

--- trees/diameter_of_a_tree.py
+++ trees/diameter_of_a_tree.py
@@ -5,14 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def get_max_depth_of_a_node(self, node, depth):
+    def __init__(self):
+        self.diameter = 0
+
+    def getDiameter(self, node):
         if node is None:
-            return depth
-        left_depth = self.get_max_depth_of_a_node(node.left, depth+1)
-        right_depth = self.get_max_depth_of_a_node(node.right, depth+1)
-        return max(left_depth, right_depth)
+            return 0
+        
+        left_height = self.getDiameter(node.left)
+        right_height = self.getDiameter(node.right)
+
+        self.diameter = max(self.diameter, left_height+right_height)
+
+        return 1 + max(left_height, right_height)
 
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        left_max_depth = self.get_max_depth_of_a_node(root.left, 0)
-        right_max_depth = self.get_max_depth_of_a_node(root.right, 0)
-        return left_max_depth+right_max_depth
+        self.getDiameter(root)
+        return self.diameter
