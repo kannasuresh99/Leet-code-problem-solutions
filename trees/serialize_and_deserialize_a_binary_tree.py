3673a4124ce383d7bf83927775a4186bf332661d
Kannappan Suresh <kannappansuresh99@gmail.com>
2023-03-22 14:27:18 +0530

    serialize and deserialize a binary tree added

--- trees/serialize_and_deserialize_a_binary_tree.py
+++ trees/serialize_and_deserialize_a_binary_tree.py
@@ -1,21 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 from collections import deque
 class Codec:
+    def __init__(self):
+        self.s = []
+
     def serialize(self, root):
         """Encodes a tree to a single string.
         
         :type root: TreeNode
         :rtype: str
         """
-        output =[]
-        def check(root):
-            if not root:
-                output.append("#")
-            if root:
-                output.append(str(root.val))
-                check(root.left)
-                check(root.right)
-        check(root)
-        return ','.join(output)
+        def serialize_helper(node):
+            if node is None:
+                self.s.append('N')
+                return
+            
+            self.s.append(str(node.val))
+            serialize_helper(node.left)
+            serialize_helper(node.right)
+        serialize_helper(root)
+        return ",".join(self.s)
+
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -23,19 +33,22 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        node_list = data.split(",")
-        node_list = deque(node_list)
-        
-        def create():
-            if node_list:
-                value = node_list.popleft()
-                if(value == "#"):
+        p = data.split(',')
+        q = deque(p)
+
+        def deserialize_helper():
+            if q:
+                node_val = q.popleft()
+                if node_val == 'N':
                     return None
-                else:
-                    node = TreeNode(int(value))
-                    node.left= create()
-                    node.right = create()
-                return node
-        
-        home = create()
-        return home
+                
+                root = TreeNode(node_val)
+                root.left = deserialize_helper()
+                root.right = deserialize_helper()
+                return root
+        return deserialize_helper()
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
