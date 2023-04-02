2ffb97225e6efd1b92527c0f070608607806fb41
Kannappan Suresh <kannappansuresh99@gmail.com>
2023-04-02 20:03:23 +0530

    island perimeter added

---
+++ island-perimeter.py
@@ -0,0 +1,16 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        perimeter = 0
+
+        for i in range(0, len(grid)):
+            for j in range(0, len(grid[0])):
+                if grid[i][j] == 1:
+                    perimeter += 4
+
+                    if i > 0 and grid[i-1][j] == 1:
+                        perimeter -= 2
+                    
+                    if j > 0 and grid[i][j-1] == 1:
+                        perimeter -= 2
+
+        return perimeter
