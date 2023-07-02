ee4c19b3793204baae05b7d4646d6d6cc86f25f2
Kannappan Suresh <kannappansuresh99@gmail.com>
2023-07-02 13:00:02 +0530

    flood fill dfs solution added

--- graphs/flood_fill.py
+++ graphs/flood_fill.py
@@ -1,6 +1,7 @@
 from collections import deque
 from typing import List
 
+#BFS solution
 class Solution:
     def bfs(self, image, row, col, visited, color):
         visited.add(tuple([row, col]))
@@ -25,3 +26,23 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         visited = set()
         return self.bfs(image, sr, sc, visited, color)
+    
+#DFS solution
+class Solution:
+    def dfs(self, image, row, col, visited, color, ref_color):
+        visited.add(tuple([row, col]))
+        image[row][col] = color
+        boundaries = [[-1,0],[1,0],[0,1],[0,-1]]
+
+        for delrow, delcol in boundaries:
+            nrow = row + delrow
+            ncol = col + delcol
+            if nrow < len(image) and ncol < len(image[0]) and nrow >= 0 and ncol >= 0 and image[nrow][ncol] == ref_color and tuple([nrow, ncol]) not in visited:
+                self.dfs(image, nrow, ncol, visited, color, ref_color)
+        return image 
+
+
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        visited = set()
+        ref_color = image[sr][sc]
+        return self.dfs(image, sr, sc, visited, color, ref_color)
