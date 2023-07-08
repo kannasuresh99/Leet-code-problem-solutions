6d1827ec1fce099bdf6b277aba061ad9e558d632
Kannappan Suresh <kannappansuresh99@gmail.com>
2023-07-08 13:31:38 +0530

    detect cycle in a graph dfs solution added

--- graphs/detect_cycle_in_a_undirected_graph.py
+++ graphs/detect_cycle_in_a_undirected_graph.py
@@ -1,6 +1,7 @@
 from typing import List
 from collections import deque
 
+#BFS solution
 class Solution:
     def detect_cycle(self, node, visited, adjList):
         visited.add(node)
@@ -28,4 +29,27 @@ class Solution:
                 if self.detect_cycle(i, visited, adj) == True:
                     return True
         
+        return False
+    
+
+#DFS solution
+
+
+class Solution:
+    def isCycle(self, V: int, adj: List[List[int]]) -> bool:
+        vis = [False]*V
+
+        def dfs(v, vis, adj, parent):
+            vis[v] = True
+            for neighbour in adj[v]:
+                if not vis[neighbour]:
+                    if dfs(neighbour, vis, adj, v):
+                        return True
+                elif parent != neighbour:
+                    return True
+            return False
+        for v in range(V):
+            if not vis[v]:
+                if dfs(v, vis, adj, -1):
+                    return True
         return False
