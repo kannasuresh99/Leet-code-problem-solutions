2db1a6d9a948ef623cb438a55e8ff9484af8f296
Kannappan Suresh <kannappansuresh99@gmail.com>
2023-07-12 15:55:08 +0530

    topological sorting added

--- graphs/detect_cycle_in_a_directed_graph.py
+++ graphs/detect_cycle_in_a_directed_graph.py
@@ -1,3 +1,4 @@
+#DFS solution
 class Solution:
     
     def dfs(self, node, visited, path_visited, adjList):
@@ -25,4 +26,35 @@ class Solution:
                     return True
                     
         return False
+
+#BFS solution
+from collections import deque
+
+class Solution:
+    
+    #Function to detect cycle in a directed graph.
+    def isCyclic(self, V, adj):
+        # code here
+        indegree = [0 for _ in range(V)]
+        topo_sort_count = 0
+        queue = deque()
+        
+        for i in range(V):
+            for adj_node in adj[i]:
+                indegree[adj_node] += 1
+                
+        for j in range(0, len(indegree)):
+            if indegree[j] == 0:
+                queue.append(j)
+                
+        while queue:
+            node_ = queue.popleft()
+            topo_sort_count += 1
+            
+            for adj_node in adj[node_]:
+                indegree[adj_node] -= 1
+                if indegree[adj_node] == 0:
+                    queue.append(adj_node)
+        
+        return False if topo_sort_count == V else True
 
--- graphs/topological_sorting.py
+++ graphs/topological_sorting.py
@@ -28,3 +28,36 @@ class Solution:
         
         return ans
 
+#BFS solution
+#Kahn's algorithm
+
+from collections import deque
+
+class Solution:
+    
+    #Function to return list containing vertices in Topological order.
+    def topoSort(self, V, adj):
+        # Code here
+        indegree = [0 for _ in range(V)]
+        ans = list()
+        queue = deque()
+        
+        for i in range(V):
+            for adj_node in adj[i]:
+                indegree[adj_node] += 1
+                
+        for j in range(0, len(indegree)):
+            if indegree[j] == 0:
+                queue.append(j)
+                
+        while queue:
+            node_ = queue.popleft()
+            ans.append(node_)
+            
+            for adj_node in adj[node_]:
+                indegree[adj_node] -= 1
+                if indegree[adj_node] == 0:
+                    queue.append(adj_node)
+        
+        return ans
+
