866c569830b4a9b7cbbaf2899bc56a910b75ca88
Kannappan Suresh <kannappansuresh99@gmail.com>
2023-07-30 18:11:59 +0530

    disjoint problems added

---
+++ graphs/number_of_operations_to_make_network_connected.py
@@ -0,0 +1,53 @@
+from typing import List
+
+class DisJoinSet:
+    def __init__(self, n):
+        self.size = [0 for _ in range(n+1)]
+        self.parent = [i for i in range(n+1)]
+
+    def find_ultimate_parent(self, node):
+        if node == self.parent[node]:
+            return node
+        
+        ultimate_parent = self.find_ultimate_parent(self.parent[node])
+        self.parent[node] = ultimate_parent
+        return self.parent[node]
+
+    def union_by_size(self, u, v):
+        ulp_u = self.find_ultimate_parent(u)
+        ulp_v = self.find_ultimate_parent(v)
+
+        if ulp_u == ulp_v:
+            return
+        
+        if self.size[ulp_u] < self.size[ulp_v]:
+            self.parent[ulp_u] = ulp_v
+            self.size[ulp_v] += self.size[ulp_u]
+        else:
+            self.parent[ulp_v] = ulp_u
+            self.size[ulp_u] += self.size[ulp_v]
+
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        extra_edges = 0
+        num_of_components = 0
+        
+        ds = DisJoinSet(n)
+
+        for edge in connections:
+            u, v = edge
+            if ds.find_ultimate_parent(u) != ds.find_ultimate_parent(v):
+                ds.union_by_size(u, v)
+            else:
+                extra_edges += 1
+        
+        for i in range(n):
+            if i == ds.parent[i]:
+                num_of_components += 1
+        
+        #minimum number of edges to connect components = number of components - 1
+        if extra_edges >= num_of_components-1:
+            return num_of_components-1
+        return -1
+    
+
--- graphs/number_of_provinces.py
+++ graphs/number_of_provinces.py
@@ -26,3 +26,66 @@ class Solution:
                 num_of_provinces += 1
         
         return num_of_provinces
+    
+#disjoint solution
+#User function Template for python3
+class DisJointSet:
+    def __init__(self, n):
+        self.rank = [0 for _ in range(n+1)]
+        self.size = [0 for _ in range(n+1)]
+        self.parent = [i for i in range(n+1)]
+        
+    def findUltimateParent(self, node):
+        #path compression
+        if node == self.parent[node]:
+            return node
+        ulp = self.findUltimateParent(self.parent[node])
+        self.parent[node] = ulp
+        return self.parent[node]
+
+    def unionByRank(self, u, v):
+        up_u = self.findUltimateParent(u)
+        up_v = self.findUltimateParent(v)
+        
+        if up_u == up_v:
+            return
+        
+        if self.rank[up_u] < self.rank[up_v]:
+            self.parent[up_u] = up_v
+        elif self.rank[up_v] < self.rank[up_u]:
+            self.parent[up_v] = up_u
+        else:
+            self.parent[u] = up_v
+            self.rank[up_v] += 1
+    
+    def unionBySize(self, u, v):
+        up_u = self.findUltimateParent(u)
+        up_v = self.findUltimateParent(v)
+        
+        if up_u == up_v:
+            return
+        
+        if self.size[up_u] < self.size[up_v]:
+            self.parent[up_u] = up_v
+            self.size[up_v] += self.size[up_u]
+        else:
+            self.parent[up_v] = up_u
+            self.size[up_u] += self.size[up_v]
+
+
+class Solution:
+    def numProvinces(self, adj, V):
+        # code here 
+        ds = DisJointSet(V)
+        number_of_provinces = 0
+        
+        for i in range(V):
+            for j in range(V):
+                if adj[i][j] == 1:
+                    ds.unionBySize(i ,j)
+                    
+        for k in range(V):
+            if ds.parent[k] == k:
+                number_of_provinces += 1
+                
+        return number_of_provinces
