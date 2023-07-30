3edb56dc09bfe92770a52722f7fa10542d244ef4
Kannappan Suresh <kannappansuresh99@gmail.com>
2023-07-30 13:46:01 +0530

    disjoint set added

---
+++ graphs/disjoint_set.py
@@ -0,0 +1,48 @@
+class DisJointSet:
+    def __init__(self, n):
+        self.rank = [0 for _ in range(n+1)]
+        self.parent = [i for i in range(n+1)]
+        
+    def findUltimateParent(self, node):
+        #path compression
+        if node == self.parent[node]:
+            return node
+        ulp = self.findUltimateParent(self.parent[node])
+        self.parent[node] = ulp
+        return self.parent[node]
+
+    def unionByRank(self, u, v):
+        up_u = self.findUltimateParent(u)
+        up_v = self.findUltimateParent(v)
+        
+        if up_u == up_v:
+            return
+        
+        if self.rank[up_u] < self.rank[up_v]:
+            self.parent[up_u] = up_v
+        elif self.rank[up_v] < self.rank[up_u]:
+            self.parent[up_v] = up_u
+        else:
+            self.parent[u] = up_v
+            self.rank[up_v] += 1
+    
+    def isBelongToSameComponent(self, u, v):
+        if self.findUltimateParent(u) == self.findUltimateParent(v):
+            print("Same")
+            return
+        else:
+            print("Not Same")
+            return
+            
+ds = DisJointSet(7)
+ds.unionByRank(1,2)
+ds.unionByRank(2,3)
+ds.unionByRank(4,5)
+ds.unionByRank(6,7)
+ds.unionByRank(5,6)
+
+ds.isBelongToSameComponent(3, 7)
+
+ds.unionByRank(3, 7)
+
+ds.isBelongToSameComponent(3, 7)
