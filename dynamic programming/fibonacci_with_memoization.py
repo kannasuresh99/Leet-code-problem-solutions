f46bbd2b39a164b23da8b8e7ec5e7f80fb245ae8
Kannappan Suresh <kannappansuresh99@gmail.com>
2023-08-05 11:07:38 +0530

    dynamic programming starting

---
+++ dynamic programming/fibonacci_with_memoization.py
@@ -0,0 +1,42 @@
+class Solution:
+    def fibonacci(self, n, dp):
+        if n <= 1:
+            return n
+        
+        if dp[n]:
+            return dp[n]
+        
+        dp[n] = self.fibonacci(n-1, dp) + self.fibonacci(n-2, dp)
+
+        return dp[n]
+    
+    def fibonacci_with_tabulation(self, n):
+        dp = [None for _ in range(n+1)]
+        dp[0] = 1
+        dp[1] = 1
+        
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n-1]
+    
+    def fibonacci_with_constant_space(self, n):
+        a = 1
+        b = 1
+        
+        for i in range(2, n+1):
+            c = a + b
+            a = b
+            b = c
+        
+        return a
+
+n = int(input("enter the nth sequence number: "))
+dp = [None for _ in range(n+1)]
+res = Solution().fibonacci(n, dp)
+print(res)
+print()
+print(Solution().fibonacci_with_tabulation(n))
+print()
+print(Solution().fibonacci_with_constant_space(n))
+
