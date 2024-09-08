85e1db3eb680f383cf0b24969ecf0ec6c44cbcb2
Kannappan Suresh <72544581+kannasuresh99@users.noreply.github.com>
2024-09-08 11:09:39 +0530

    A2Z problems started

--- arrays/striver_sheet_problems/easy/remove_duplicatea_from_a_sorted_array.py
+++ arrays/striver_sheet_problems/easy/remove_duplicatea_from_a_sorted_array.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class Solution:
-    def check(self, nums: List[int]) -> bool:
+    def removeDuplicates(self, nums: List[int]) -> bool:
         i = 0
         for j in range(1, len(nums)):
             if nums[j] == nums[i]:
@@ -11,5 +11,5 @@ class Solution:
             i += 1
 
 nums = [1,1,1,2,2,3,3,3,3,3,4,4,4,4]
-Solution().check(nums)
-print(nums)
+res = Solution().removeDuplicates(nums)
+print(nums[:res])
